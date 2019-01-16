d7bf71b08a4ae715d0d9269662f2e4339b4d3a6e
caiismyname <davidcai2012@gmail.com>
2019-01-15 23:02:08 -0600

    Camera view shows up

--- v-log/ViewController.swift
+++ v-log/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
+
+var captureSession: AVCaptureSession?
+var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewfinder: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            viewfinder.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+            
+        } catch {
+            print(error)
+        }
+        
+        
+        
     }
-
+    
+    
 
 }
 
