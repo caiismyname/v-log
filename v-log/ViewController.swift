194fa5dcbcf96f433004c32c3e0d1dbb90224af3
caiismyname <davidcai2012@gmail.com>
2019-01-30 23:38:32 -0600

    Switched camera to video (saving to camera roll), button switches colors when recording

--- v-log/ViewController.swift
+++ v-log/ViewController.swift
@@ -8,68 +8,136 @@
 
 import UIKit
 import AVFoundation
+import AVKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var viewfinder: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var viewFinder: UIView!
+
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var capturePhotoOutput: AVCapturePhotoOutput?
-
+    var captureVideoOutput: AVCaptureMovieFileOutput?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
         
         let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
         do {
             let input = try AVCaptureDeviceInput(device: captureDevice!)
             captureSession = AVCaptureSession()
             captureSession?.addInput(input)
-            
+
             videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
             videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
             videoPreviewLayer?.frame = view.layer.bounds
-            viewfinder.layer.addSublayer(videoPreviewLayer!)
-            
+            videoPreviewLayer?.session = captureSession
+            viewFinder.layer.addSublayer(videoPreviewLayer!)
+            // Make sure to bring the button to front *after* adding the video preview
+            self.view.bringSubviewToFront(recordButton)
+
             captureSession?.startRunning()
-            
+
             // Prepare capture button
-            capturePhotoOutput = AVCapturePhotoOutput()
-            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
-            captureSession?.addOutput(capturePhotoOutput!)
+            captureVideoOutput =  AVCaptureMovieFileOutput()
+            captureSession?.addOutput(captureVideoOutput!)
         } catch {
             print(error)
         }
     }
     
-    @IBAction func button(_ sender: Any) {
+    @IBAction func recordButtonPress(_ sender: Any) {
+        guard let captureVideoOutput = self.captureVideoOutput else { return}
         
-        guard let capturePhotoOutput = self.capturePhotoOutput else { return}
-        
-        let photoSettings = AVCapturePhotoSettings()
-        photoSettings.isAutoStillImageStabilizationEnabled = true
-        photoSettings.isHighResolutionPhotoEnabled = true
-        photoSettings.flashMode = .off
-        
-        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+        if captureVideoOutput.isRecording {
+            // Stop
+            captureVideoOutput.stopRecording()
+            recordButton.setTitle("Record", for: [])
+            recordButton.setTitleColor(UIColor.red, for: [])
+            recordButton.backgroundColor = UIColor.white
+            
+            print("stoppping the recording")
+        } else {
+            // Start
+            let outputFileName = NSUUID().uuidString
+            let outputFilePath = (NSTemporaryDirectory() as NSString).appendingPathComponent((outputFileName as NSString).appendingPathExtension("mov")!)
+            captureVideoOutput.startRecording(to: URL(fileURLWithPath: outputFilePath), recordingDelegate: self)
+            
+            recordButton.setTitle("Stop", for: [])
+            recordButton.setTitleColor(UIColor.white, for: [])
+            recordButton.backgroundColor = UIColor.red
+            
+            print("starting to record")
+        }
+    }
+    
+    
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
+//
+//extension ViewController : AVCapturePhotoCaptureDelegate {
+//    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+//        // Get captured image
+//        guard error == nil else {
+//            print("error capturing photo")
+//            return
+//        }
+//
+//        guard let imageData = photo.fileDataRepresentation() else { return }
+//
+//        let capturedImage = UIImage.init(data: imageData, scale: 1.0)
+//        if let image = capturedImage {
+//            UIImageWriteToSavedPhotosAlbum(image, nil, nil
+//                , nil)
+//        }
+//
+//    }
+//}
 
-extension ViewController : AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        // Get captured image
-        guard error == nil else {
-            print("error capturing photo")
-            return
+extension ViewController : AVCaptureFileOutputRecordingDelegate {
+    // DidStartRecording
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        // Enable the Record button to let the user stop recording.
+    }
+    
+    // DidFinishRecording
+    func fileOutput(_ output: AVCaptureFileOutput,
+                    didFinishRecordingTo outputFileURL: URL,
+                    from connections: [AVCaptureConnection],
+                    error: Error?) {
+        // Note: Since we use a unique file path for each recording, a new recording won't overwrite a recording mid-save.
+        func cleanup() {
+            let path = outputFileURL.path
+            if FileManager.default.fileExists(atPath: path) {
+                do {
+                    try FileManager.default.removeItem(atPath: path)
+                } catch {
+                    print("Could not remove file at url: \(outputFileURL)")
+                }
+            }
         }
-
-        guard let imageData = photo.fileDataRepresentation() else { return }
+        var success = true
         
-        let capturedImage = UIImage.init(data: imageData, scale: 1.0)
-        if let image = capturedImage {
-            UIImageWriteToSavedPhotosAlbum(image, nil, nil
-                , nil)
+        if error != nil {
+            print("Movie file finishing error: \(String(describing: error))")
+            success = (((error! as NSError).userInfo[AVErrorRecordingSuccessfullyFinishedKey] as AnyObject).boolValue)!
         }
-
+        
+        if success {
+            // TODO Check authorization status.
+            print("saving")
+            UISaveVideoAtPathToSavedPhotosAlbum(outputFileURL.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+            
+        }
     }
 }
-
