58371870e0d52d848c744852cc97617e33519fd6
caiismyname <davidcai2012@gmail.com>
2019-02-01 00:15:31 -0600

    Clips now save to documents directory, not photos album. Also, I learned how `FileManager` and `URL`s work

--- v-log/ViewController.swift
+++ v-log/ViewController.swift
@@ -23,8 +23,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
         let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
         do {
             let input = try AVCaptureDeviceInput(device: captureDevice!)
@@ -61,11 +59,10 @@ class ViewController: UIViewController {
             
             print("stoppping the recording")
         } else {
-            // Start
+            // TODO Don't think this timestamp thing will be robust to timezone issues
             let outputFileName = NSUUID().uuidString
             let outputFilePath = (NSTemporaryDirectory() as NSString).appendingPathComponent((outputFileName as NSString).appendingPathExtension("mov")!)
             captureVideoOutput.startRecording(to: URL(fileURLWithPath: outputFilePath), recordingDelegate: self)
-            
             recordButton.setTitle("Stop", for: [])
             recordButton.setTitleColor(UIColor.white, for: [])
             recordButton.backgroundColor = UIColor.red
@@ -75,6 +72,7 @@ class ViewController: UIViewController {
     }
     
     
+//    Callback that displays the "success" popup
     @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
         let title = (error == nil) ? "Success" : "Error"
         let message = (error == nil) ? "Video was saved" : "Video failed to save"
@@ -83,61 +81,56 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
+    
+    func getDirectoryPath() -> URL {
+        do {
+            let fmanager = FileManager.default
+            let documents = fmanager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let vlog_folder = documents.appendingPathComponent("vlog_clips", isDirectory: true)
+            
+            // First, create the directory
+            var isDirectory: ObjCBool = false
+            let directoryExists = fmanager.fileExists(atPath: vlog_folder.path, isDirectory: &isDirectory)
+            if (!directoryExists || !isDirectory.boolValue) {
+                try fmanager.createDirectory(at: vlog_folder, withIntermediateDirectories: false, attributes: [:])
+                print("directory didn't exist, making it")
+            }
+            
+            let clip_url = vlog_folder.appendingPathComponent(generateCurrentTimeStamp(), isDirectory: false).appendingPathExtension("mov")
+            return clip_url
+        } catch {
+            print("error while creating clip url")
+            return URL(string: "error")!
+        }
+    }
+    
+    func generateCurrentTimeStamp () -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy_MM_dd_hh_mm_ss"
+        return (formatter.string(from: Date()) as NSString) as String
+    }
 }
-//
-//extension ViewController : AVCapturePhotoCaptureDelegate {
-//    func photoOutput(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-//        // Get captured image
-//        guard error == nil else {
-//            print("error capturing photo")
-//            return
-//        }
-//
-//        guard let imageData = photo.fileDataRepresentation() else { return }
-//
-//        let capturedImage = UIImage.init(data: imageData, scale: 1.0)
-//        if let image = capturedImage {
-//            UIImageWriteToSavedPhotosAlbum(image, nil, nil
-//                , nil)
-//        }
-//
-//    }
-//}
 
 extension ViewController : AVCaptureFileOutputRecordingDelegate {
     // DidStartRecording
     func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
-        // Enable the Record button to let the user stop recording.
     }
     
     // DidFinishRecording
-    func fileOutput(_ output: AVCaptureFileOutput,
-                    didFinishRecordingTo outputFileURL: URL,
-                    from connections: [AVCaptureConnection],
-                    error: Error?) {
-        // Note: Since we use a unique file path for each recording, a new recording won't overwrite a recording mid-save.
-        func cleanup() {
-            let path = outputFileURL.path
-            if FileManager.default.fileExists(atPath: path) {
-                do {
-                    try FileManager.default.removeItem(atPath: path)
-                } catch {
-                    print("Could not remove file at url: \(outputFileURL)")
-                }
-            }
-        }
-        var success = true
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
         
         if error != nil {
             print("Movie file finishing error: \(String(describing: error))")
-            success = (((error! as NSError).userInfo[AVErrorRecordingSuccessfullyFinishedKey] as AnyObject).boolValue)!
+//            success = (((error! as NSError).userInfo[AVErrorRecordingSuccessfullyFinishedKey] as AnyObject).boolValue)!
         }
         
-        if success {
-            // TODO Check authorization status.
-            print("saving")
-            UISaveVideoAtPathToSavedPhotosAlbum(outputFileURL.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
-            
+        // Save video to documents directory
+        do {
+            try FileManager.default.moveItem(at: outputFileURL, to: getDirectoryPath())
+            print("done saving")
+        } catch {
+            print("Error saving video")
         }
+        
     }
 }
